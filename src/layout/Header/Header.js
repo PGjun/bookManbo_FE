75699934797d6b8d741fe89cb79f46f20db6b6e5
Lenson-Lee <dmstjs7437@naver.com>
2023-01-10 21:48:32 +0900

    Layout, Main, Router확인요망

---
+++ src/layout/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header className="bg-yellow-200 h-10 w-full"></header>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/page/Main/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Main = (props) => {
+  return (
+    <>
+      <div className="max-w-screen-2xl mx-auto bg-blue-50 w-full h-full">
+        AA
+      </div>
+    </>
+  );
+};
+
+export default Main;

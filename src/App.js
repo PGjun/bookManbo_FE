8015f4471c9c6776a1c315c71c70208e9ea1be1a
Lenson-Lee <dmstjs7437@naver.com>
2023-01-10 20:48:32 +0900

    first setting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import tw from "twin.macro";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const Test = styled.main.attrs({
+    className: "my-20 p-4 w-96 border bg-blue-500",
+  })`
+    & {
+      .test {
+        ${tw`bg-pink-500`}
+      }
+      span {
+        ${tw`bg-white`}
+      }
+    }
+  `;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <Test>
+        <div></div>
+      </Test>
+      <Routes></Routes>
     </div>
   );
 }
